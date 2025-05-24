5a3583ac202f1e3750dac9a61699b880c80cc0ad
JRVaccaro <ooliebud@gmail.com>
2025-05-24 19:45:40 -0400

    Added constructor and getters/setters to Sandwich class

--- src/main/java/com/pluralsight/sandwichmodels/Sandwich.java
+++ src/main/java/com/pluralsight/sandwichmodels/Sandwich.java
@@ -10,6 +10,63 @@ public class Sandwich extends Item  {
     private boolean hasExtraCheese;
     private boolean hasExtraMeat;
 
+    public Sandwich(String size, Bread bread, boolean isToasted, List<Toppings> toppings, boolean hasExtraCheese, boolean hasExtraMeat) {
+        this.size = size;
+        this.bread = bread;
+        this.isToasted = isToasted;
+        this.toppings = toppings;
+        this.hasExtraCheese = hasExtraCheese;
+        this.hasExtraMeat = hasExtraMeat;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Bread getBread() {
+        return bread;
+    }
+
+    public void setBread(Bread bread) {
+        this.bread = bread;
+    }
+
+    public boolean isToasted() {
+        return isToasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        isToasted = toasted;
+    }
+
+    public List<Toppings> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<Toppings> toppings) {
+        this.toppings = toppings;
+    }
+
+    public boolean isHasExtraCheese() {
+        return hasExtraCheese;
+    }
+
+    public void setHasExtraCheese(boolean hasExtraCheese) {
+        this.hasExtraCheese = hasExtraCheese;
+    }
+
+    public boolean isHasExtraMeat() {
+        return hasExtraMeat;
+    }
+
+    public void setHasExtraMeat(boolean hasExtraMeat) {
+        this.hasExtraMeat = hasExtraMeat;
+    }
+
     @Override
     public double getPrice() {
 
