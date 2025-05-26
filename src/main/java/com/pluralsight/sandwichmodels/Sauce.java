8d893aafd4d897df0585c9f82919d39475252604
JRVaccaro <ooliebud@gmail.com>
2025-05-26 14:05:39 -0400

    Implemented Sauce class with type constructor, and getPrice method returning 0

--- src/main/java/com/pluralsight/sandwichmodels/Sauce.java
+++ src/main/java/com/pluralsight/sandwichmodels/Sauce.java
@@ -1,4 +1,13 @@
 package com.pluralsight.sandwichmodels;
 
-public class Sauce {
+public class Sauce extends Toppings{
+    public Sauce(String type) {
+        super(type);
+    }
+
+    @Override
+    public double getPrice() {
+        //no additional cost for sauce, it's included in price.
+        return 0;
+    }
 }
