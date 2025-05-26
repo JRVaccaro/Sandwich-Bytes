1503dbeb06b00464eaaeb84d29d6230dc52efc0f
JRVaccaro <ooliebud@gmail.com>
2025-05-26 16:16:14 -0400

    Implemented Meat class with constructor and getPrice method. Pricing will be handled in Sandwich class

--- src/main/java/com/pluralsight/sandwichmodels/Meat.java
+++ src/main/java/com/pluralsight/sandwichmodels/Meat.java
@@ -1,4 +1,13 @@
 package com.pluralsight.sandwichmodels;
 
 public class Meat extends PremiumTopping{
+    public Meat(String type) {
+        super(type);
+    }
+
+    @Override
+    public double getPrice() {
+        //Price will be determined in Sandwich class based on sandwich size.
+        return 0;
+    }
 }
