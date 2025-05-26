e93d24bffca02f753e889f5bbec9f45b8652d6a7
JRVaccaro <ooliebud@gmail.com>
2025-05-26 13:51:52 -0400

    Implemented RegularTopping class with type constructor, and getPrice method returning 0

--- src/main/java/com/pluralsight/sandwichmodels/RegularTopping.java
+++ src/main/java/com/pluralsight/sandwichmodels/RegularTopping.java
@@ -1,4 +1,15 @@
 package com.pluralsight.sandwichmodels;
 
 public class RegularTopping extends Toppings{
+    public RegularTopping(String type) {
+        super(type);
+    }
+
+    @Override
+    public double getPrice() {
+        //no additional cost for regular toppings, it's included in price
+        return 0;
+
+    }
 }
+
