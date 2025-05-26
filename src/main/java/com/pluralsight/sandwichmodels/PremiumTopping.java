7e54f67cdaea7718ca7df14fec7532881f0a75fd
JRVaccaro <ooliebud@gmail.com>
2025-05-26 14:24:39 -0400

    Implemented abstract PremiumTopping class with constructor, and abstracr getPrice method

--- src/main/java/com/pluralsight/sandwichmodels/PremiumTopping.java
+++ src/main/java/com/pluralsight/sandwichmodels/PremiumTopping.java
@@ -1,4 +1,12 @@
 package com.pluralsight.sandwichmodels;
 
 public abstract class PremiumTopping extends Toppings{
-}
+    public PremiumTopping(String type) {
+        super(type);
+    }
+
+    @Override
+    public abstract double getPrice();
+
+    }
+
