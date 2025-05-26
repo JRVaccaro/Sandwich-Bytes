cc35a85840b16375b4d274ca7545407036fde51c
JRVaccaro <ooliebud@gmail.com>
2025-05-26 13:04:23 -0400

    Implemented Chips class with constructor, getters/setters. Set the price for chips in getPrice method

--- src/main/java/com/pluralsight/sandwichmodels/Chips.java
+++ src/main/java/com/pluralsight/sandwichmodels/Chips.java
@@ -1,8 +1,33 @@
 package com.pluralsight.sandwichmodels;
 
 public class Chips extends Item{
+    private String size;
+    private String flavor;
+
+    public Chips(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
     @Override
     public double getPrice() {
+    return 1.50;
 
     }
 }
