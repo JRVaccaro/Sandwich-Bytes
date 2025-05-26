37c1af72eb06c87fbcddf7ce4d1b6b13f07ef705
JRVaccaro <ooliebud@gmail.com>
2025-05-26 13:17:30 -0400

    Implemented abstract Toppings class with type field, constructor, getter/setter, and abstract getPrice method

--- src/main/java/com/pluralsight/sandwichmodels/Toppings.java
+++ src/main/java/com/pluralsight/sandwichmodels/Toppings.java
@@ -3,6 +3,21 @@ package com.pluralsight.sandwichmodels;
 import com.pluralsight.interfaces.Priceable;
 
 public abstract class Toppings implements Priceable {
+    private String type;
+
+    public Toppings(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    //abstract method to get the price of the topping, which will be used in all subclasses
     @Override
     public abstract double getPrice();
     }
