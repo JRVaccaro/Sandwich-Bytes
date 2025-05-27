4c67abf8987f4f3374187c8c87c644293a88dce3
JRVaccaro <ooliebud@gmail.com>
2025-05-27 16:41:26 -0400

    Implemented Receipt class to create receipts and file saving with timestamp names.

--- src/main/java/com/pluralsight/orders/Receipt.java
+++ src/main/java/com/pluralsight/orders/Receipt.java
@@ -2,6 +2,9 @@ package com.pluralsight.orders;
 
 import com.pluralsight.sandwichmodels.Item;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -13,8 +16,10 @@ private LocalDateTime timestamp;
         this.order = order;
         this.timestamp = LocalDateTime.now();
     }
-    public String generateReceiptText(){
+    public String generateReceiptText() {
         StringBuilder sb = new StringBuilder();
+
+        double total = 0;
 
 
         //Header
@@ -22,9 +27,27 @@ private LocalDateTime timestamp;
         sb.append("--- Receipt ---\n");
         sb.append(timestamp.format(formatter));
 
-        //need to list items and prices
-        for (Item item : order.getItems()){
-            // think i should make to String methods? sb.append()
+        //Listing items and prices
+        for (Item item : order.getItems()) {
+            sb.append(item.toString()).append("\n");
+            total += item.getPrice();
+
         }
+        sb.append("Total: $").append(String.format("%.2f", total));
+        return sb.toString();
+    }
+    public void saveReceiptToFile(){
+        DateTimeFormatter fileFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String filename = "receipts/" + timestamp.format(fileFormatter) + ".txt";
+        try {
+            FileWriter writer = new FileWriter(filename);
+            writer.write(generateReceiptText());
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Failed to create receipt");
+        }
+
+
     }
 }
