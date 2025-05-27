3b229b3218a9e90c54dd2a98b1e060bcedf68110
JRVaccaro <ooliebud@gmail.com>
2025-05-27 17:54:18 -0400

    Added order menu options and switch logic to processNewOrder method.

--- src/main/java/com/pluralsight/userinterface/UserInterface.java
+++ src/main/java/com/pluralsight/userinterface/UserInterface.java
@@ -8,14 +8,14 @@ public class UserInterface {
     private Scanner scanner;
     private Order order;
 
-    public UserInterface(){
+    public UserInterface() {
         scanner = new Scanner(System.in);
     }
 
-    public void display(){
+    public void display() {
 
         boolean exit = false;
-        while(!exit) {
+        while (!exit) {
 
             System.out.println("Welcome to Sandwich Bytes!");
             System.out.println("1) New Order");
@@ -38,7 +38,49 @@ public class UserInterface {
             }
         }
     }
-    public void processNewOrder(){
 
+    public void processNewOrder() {
+        boolean ordering = true;
+
+        while (ordering) {
+
+            System.out.println("Order Menu:");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) CHECKOUT");
+            System.out.println("0) Cancel Order");
+
+            System.out.println("Please choose an option: ");
+            String option = scanner.nextLine();
+
+            switch (option) {
+
+                case "1":
+                    //call to method
+                    break;
+
+                case "2":
+                    //call to method
+                    break;
+
+                case "3":
+                    //call to method
+                    break;
+
+                case "4":
+                    //call to method
+                    break;
+
+                case "0":
+                    System.out.println("Order has been cancelled.");
+                    ordering = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+        }
     }
 }
