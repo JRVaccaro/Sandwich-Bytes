3e7bd997bfa6ac7938df6cae8d7c7f5f73a98530
JRVaccaro <ooliebud@gmail.com>
2025-05-28 16:38:34 -0400

    Implemented Main class to launch sandwich shop application.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
+import com.pluralsight.userinterface.UserInterface;
+
 public class Main {
+    public static void main(String[] args) {
+
+        UserInterface ui = new UserInterface();
+
+        ui.display();
+    }
 }
