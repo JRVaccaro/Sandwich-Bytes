54b46a434a84b27e436b2884c0cd4c044663eb86
JRVaccaro <ooliebud@gmail.com>
2025-05-28 16:31:12 -0400

    Removed OrderTest class, replaced with BreadTest class.

---
+++ src/test/java/com/pluralsight/sandwichmodels/BreadTest.java
@@ -0,0 +1,12 @@
+package com.pluralsight.sandwichmodels;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreadTest {
+
+    @Test
+    void getType() {
+    }
+}
