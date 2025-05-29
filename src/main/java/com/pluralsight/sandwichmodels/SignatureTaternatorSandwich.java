3aa95843cf743c93e1f1a8c71cdf7986b90d57a8
JRVaccaro <ooliebud@gmail.com>
2025-05-29 10:15:34 -0400

    Added constructor and custom toppings to SignatureTaterNatorSandwich.

--- src/main/java/com/pluralsight/sandwichmodels/SignatureTaternatorSandwich.java
+++ src/main/java/com/pluralsight/sandwichmodels/SignatureTaternatorSandwich.java
@@ -1,4 +1,21 @@
 package com.pluralsight.sandwichmodels;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SignatureTaternatorSandwich extends Sandwich{
+    public SignatureTaternatorSandwich() {
+        super(12, new Bread("Potato bread"), true, taternatorToppings(), false, false);
+    }
+    private static List<Toppings> taternatorToppings(){
+        List<Toppings> toppings = new ArrayList<>();
+        toppings.add(new Meat("Bacon"));
+        toppings.add(new Cheese("Pepper Jack"));
+        toppings.add(new RegularTopping("Tater Tots"));
+        toppings.add(new Sauce("Buffalo"));
+
+        return toppings;
+
+
+    }
 }
